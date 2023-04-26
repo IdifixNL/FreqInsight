c7237640656f8fdf2c1a8d3222e93f30848b97f5
Nico <nico_baburk@hotmail.com>
2023-04-26 17:22:02 +0200

    Fix Stragey pull down for Backtest/ Added datefield for backtest

--- backtest.py
+++ backtest.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+from tkinter import ttk
 import subprocess
 import os
+import re
 import config
 
 def run_backtest():
@@ -9,9 +11,23 @@ def run_backtest():
     selected_intervals = [interval for interval, var in zip(intervals, interval_vars) if var.get()]
     intervals_str = " ".join([f"--timeframe {interval}" for interval in selected_intervals])
     strategy = strategy_var.get()
-    command = f"docker-compose run --rm -v {location}:/freqtrade/user_data freqtrade backtesting --strategy {strategy} --timerange=20230101- {intervals_str}"
+    timerange = timerange_entry.get() + "-"
+    command = f"docker-compose run --rm -v {location}:/freqtrade/user_data freqtrade backtesting --strategy {strategy} --timerange={timerange} {intervals_str}"
     os.chdir(location)
     subprocess.Popen(['xterm', '-hold', '-e', command])
+
+def get_available_strategies():
+    strategies_path = config.strategies_path
+    strategy_files = os.listdir(strategies_path)
+    available_strategies = []
+    for file in strategy_files:
+        with open(os.path.join(strategies_path, file), 'r') as f:
+            content = f.read()
+            match = re.search(r'class\s+(.*?)\s*\(\s*IStrategy', content)
+            if match:
+                strategy_name = match.group(1)
+                available_strategies.append(strategy_name)
+    return available_strategies
 
 root = tk.Tk()
 root.title("Backtest")
@@ -25,6 +41,9 @@ interval_frame.pack()
 strategy_frame = tk.Frame(root, padx=10, pady=10)
 strategy_frame.pack()
 
+timerange_frame = tk.Frame(root, padx=10, pady=10)
+timerange_frame.pack()
+
 backtest_button = tk.Button(backtest_frame, text="Run Backtest", command=run_backtest)
 backtest_button.pack()
 
@@ -34,11 +53,16 @@ for i, interval in enumerate(intervals):
     cb = tk.Checkbutton(interval_frame, text=interval, variable=interval_vars[i])
     cb.pack(side=tk.LEFT)
 
-strategies_path = "/home/nico/Documents/projects/trader/dev-freq/ft_userdata/user_data/strategies"
-strategies = os.listdir(strategies_path)
+strategy_options = get_available_strategies()
 strategy_var = tk.StringVar()
-strategy_var.set(strategies[0])
-strategy_dropdown = tk.OptionMenu(strategy_frame, strategy_var, *strategies)
+strategy_var.set(strategy_options[0])
+strategy_dropdown = ttk.OptionMenu(strategy_frame, strategy_var, strategy_options[0], *strategy_options)
 strategy_dropdown.pack()
+
+timerange_label = tk.Label(timerange_frame, text="Timerange")
+timerange_label.pack(side=tk.LEFT)
+
+timerange_entry = tk.Entry(timerange_frame, width=20)
+timerange_entry.pack(side=tk.LEFT)
 
 root.mainloop()

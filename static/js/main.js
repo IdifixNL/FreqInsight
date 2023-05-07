1b9dd126c576d6aea6c95ec5fb7b9a9b2c79a7bb
Nico <nico_baburk@hotmail.com>
2023-05-07 21:15:38 +0200

    working on splittinng html files

--- static/js/main.js
+++ static/js/main.js
@@ -1,60 +1,63 @@
-// Function to show the section with the given id and hide others
-function showSection(id) {
+$(document).ready(function() {
+  // Hide all sections except the first one
+  $(".section:not(:first)").hide();
+
+  // Function to show the section with the given id and hide others
+  function showSection(id) {
     $(".section").hide();
     $("#" + id).show();
   }
-  
+
   // Event listeners for menu buttons
   $('#backtest-btn').click(function() {
     showSection('backtest');
   });
-  
+
   $('#hyperopt-btn').click(function() {
     showSection('hyperopt');
   });
-  
+
   $('#datadownload-btn').click(function() {
     showSection('datadownload');
   });
-  
+
   $('#configuration-btn').click(function() {
     showSection('configuration');
   });
-  
+
   $('#help-btn').click(function() {
     showSection('help');
   });
-  
+
   $('#test-btn').click(function() {
-    // Hide the "Test" button in the navbar
-    $(this).hide();
-  
     // Show the "Test" button in the "Test" section
     $('#test-section-button').show();
-  
+
     // Show the "Test" section
     showSection('test');
   });
-  
+
   $('#test-section-button').click(function() {
     $.post('/test')
-      .done(function(data) {
-        $('#test-result').text("Message: " + data.message + "\nName: " + data.name + "\nState: " + data.state);
-      })
-      .fail(function(jqXHR, textStatus, errorThrown) {
-        $('#test-result').text('Error: ' + errorThrown);
-      });
+        .done(function(data) {
+            $('#test-result').text("Message: " + data.message + "\nName: " + data.name + "\nState: " + data.state);
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            $('#test-result').text('Error: ' + errorThrown);
+        });
   });
-  
+
   $('#logs-btn').click(function() {
     $.get('/logs')
-      .done(function(data) {
-        $('#logs-content').html(data);
-        showSection('logs');
-      })
-      .fail(function(jqXHR, textStatus, errorThrown) {
-        $('#logs-content').text('Error: ' + errorThrown);
-        showSection('logs');
-      });
+        .done(function(data) {
+            $('#logs-content').html(data);
+            showSection('logs');
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            $('#logs-content').text('Error: ' + errorThrown);
+            showSection('logs');
+        });
   });
-  
+
+  showSection('configuration');
+});

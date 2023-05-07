0f2cf0f9ac2db7552776c0587f14b3509efdf8d8
Nico <nico_baburk@hotmail.com>
2023-05-07 02:32:03 +0200

    now whit css functionality

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-# Import the necessary libraries
 import subprocess
 import json
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, jsonify
 
 # Create the Flask application
 app = Flask(__name__)
 
-# Define the route for the home page
 @app.route('/')
 def home(message=None, output=None):
     return render_template('index.html', message=message, output=output)
 
-# Define the route for the about page
 @app.route('/about')
 def about():
     return "This is the About page."
@@ -53,6 +50,5 @@ def test():
 
     return {'message': message, 'name': name, 'state': state, 'raw_output': output}
 
-# Run the application
 if __name__ == '__main__':
     app.run(debug=True)

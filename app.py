c8f895fe9b7c5e8110fb213bf8534f9b2c029590
Nico <nico_baburek@hotmail.com>
2023-05-08 23:45:21 +0200

    test

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from config import __init__
 from flask import Flask, render_template
 from routes import main_bp
+from flask import request
 
 app = Flask(__name__)
 app.register_blueprint(main_bp)
@@ -14,3 +15,10 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
+
+
+@app.route('/profile', methods=['POST'])
+def profile():
+    data = request.form['data']
+    # process data here
+    return 'Profile endpoint'
